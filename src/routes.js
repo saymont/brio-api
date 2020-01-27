603d1a71a5fd501a8eae12a7e25e548f145f2c18
saymonp <saymonp.trevisan@gmail.com>
2020-01-27 14:25:53 -0300

    Obtem Postagens com o cliente da API do WordPress

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,13 @@ const authMiddleware = require("./middlewares/auth");
 
 const routes = express.Router();
 
+const PostController = require("./controllers/PostController");
+
 const SessionController = require("./controllers/SessionController");
 const PsychologistController = require("./controllers/PsychologistController");
 const DashboardController = require("./controllers/DashboardController");
+
+routes.get("/api/v1/posts", PostController.getPosts);
 
 routes.post("/api/v1/login", SessionController.login);
 routes.post("/api/v1/register", SessionController.registerUser);
